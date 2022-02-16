5d3577c0b0c57bb5ab96d5febb51259d9f8cf85f
Andres J. Jiménez Leandro <Andres.Leandro@forsyteit.com>
2022-02-15 18:48:47 -0600

    Add project files.

---
+++ CosmosDBAPI/Controllers/ItemController.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CosmosDBAPI.Models;
+namespace CosmosDBAPI.Controllers
+{
+    public class ItemController : Controller
+    {
+        private readonly ICosmosDBService _cosmosDbService;
+        public ItemController(ICosmosDBService cosmosDbService)
+        {
+            _cosmosDbService = cosmosDbService;
+        }
+        [HttpGet]
+        [Route("Index")]
+        public async Task<List<Item>> Index()
+        {
+            try
+            {
+                return (await _cosmosDbService.GetItemsAsync("SELECT * FROM c")).ToList(); ;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+        [HttpPost]
+        [Route("CreateMine")]
+        public async Task Create(Item item)
+        {
+            try
+            {
+                await _cosmosDbService.AddItemAsync(item);
+            }
+            catch { throw; }
+        }
+        [HttpPost]
+        [Route("Create")]
+        public async Task CreateAsync([Bind("Id,Name,Description,Completed")] Item item)
+        {
+            try
+            {
+                item.Id = Guid.NewGuid().ToString();
+                await _cosmosDbService.AddItemAsync(item);
+            }
+            catch { throw; }
+        }
+        [HttpDelete]
+        [Route("Delete")]
+        public async Task Delete([Bind("Id,Name,Description,Completed")] Item item)
+        {
+            try
+            {
+                await _cosmosDbService.DeleteItemAsync(item.Id);
+            }
+            catch { throw;  }
+        }
+        [HttpPost] [Route("Edit")]
+        public async Task<ActionResult> EditAsync([Bind("Id,Name,Description,Completed")] Item item)
+        {
+            if (ModelState.IsValid)
+            {
+                await _cosmosDbService.UpdateItemAsync(item.Id, item);
+                return RedirectToAction("Index");
+            }
+            return View(item);
+        }
+
+        [ActionName("Edit")]
+        public async Task<ActionResult> EditAsync(string id)
+        {
+            if (id == null)
+            {
+                return BadRequest();
+            }
+            Item item = await _cosmosDbService.GetItemAsync(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return View(item);
+        }
+        [ActionName("Delete")]
+        public async Task<ActionResult> DeleteAsync(string id)
+        {
+            if (id == null)
+            {
+                return BadRequest();
+            }
+            Item item = await _cosmosDbService.GetItemAsync(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return View(item);
+        }
+        [HttpPost]
+        [ActionName("Delete")]
+        public async Task<ActionResult> DeleteConfirmedAsync([Bind("Id")] string id)
+        {
+            await _cosmosDbService.DeleteItemAsync(id);
+            return RedirectToAction("Index");
+        }
+        [ActionName("Details")]
+        public async Task<ActionResult> DetailsAsync(string id)
+        {
+            return View(await _cosmosDbService.GetItemAsync(id));
+        }
+    }
+}
---
+++ CosmosDBAPI/Startup.cs
@@ -0,0 +1,74 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CosmosDBAPI.Models;
+using Microsoft.OpenApi.Models;
+/// <summary>
+/// This is the StartUp Class for the CosmosDBAPI project
+/// </summary>
+namespace CosmosDBAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+            services.AddSingleton<ICosmosDBService>(InitializeCosmosClientInstanceAsync(Configuration.GetSection("CosmosDb")).GetAwaiter().GetResult());
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "ApiCosmosDB", Version = "v1" });
+            });
+        }
+        //Agregar Tareas de Cosmos
+        private static async Task<CosmosDBService> InitializeCosmosClientInstanceAsync(IConfigurationSection configurationSection)
+        {
+            string databaseName = configurationSection.GetSection("DatabaseName").Value;
+            string containerName = configurationSection.GetSection("ContainerName").Value;
+            string account = configurationSection.GetSection("Account").Value;
+            string key = configurationSection.GetSection("Key").Value;
+            Microsoft.Azure.Cosmos.CosmosClient client = new Microsoft.Azure.Cosmos.CosmosClient(account, key);
+            CosmosDBService cosmosDbService = new CosmosDBService(client, databaseName, containerName);
+            Microsoft.Azure.Cosmos.DatabaseResponse database = await client.CreateDatabaseIfNotExistsAsync(databaseName);
+            await database.Database.CreateContainerIfNotExistsAsync(containerName, "/id");
+
+            return cosmosDbService;
+        }
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ApiCosmosDB v1"));
+            }
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                        name: "default",
+                        pattern: "{controller=Item}/{action=Index}/{id?}"
+                    );
+            });
+        }
+    }
+}
